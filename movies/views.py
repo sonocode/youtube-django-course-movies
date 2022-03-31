4e03231c77816b16a44b90dfbeb5d11148da07a1
sonocode <help@mcluxe.com>
2022-03-30 20:57:43 -0400

    Configure views and templates

---
+++ movies/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+data = {
+    "movies": [
+        {"id": 5, "title": "Jaws", "year": 1669},
+        {"id": 6, "title": "Matrix", "year": 1999},
+        {"id": 7, "title": "Speed", "year": 2000},
+    ]
+}
+
+
+def movies(request):
+    return render(request, "movies/movies.html", data)
+
+
+def home(request):
+    return HttpResponse("Home Page")
